d3bd2c9fb9eca9150081f05b9590396e0a6f2aee
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-05-22 16:21:32 +1000

    XMLParser: Started parser class for XML files

---
+++ 2D-Map-Editor-master/src/XMLParser.java
@@ -0,0 +1,11 @@
+/**
+ * XML Parser is a class meant to extract all the items, enemies, and pacActor
+ * and their locations and store it in a hashmap
+ */
+public class XMLParser {
+
+
+    public XMLParser() {
+
+    }
+}
