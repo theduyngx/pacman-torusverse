ad987839bcb801c26b4d415661330b3427cdad06
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-05-22 15:10:43 +1000

    Created first version of Portal and PortalFactory

--- pacman/src/Portal.java
+++ pacman/src/Portal.java
@@ -1,20 +1,26 @@
 /**
- * Pill class extended from abstract Item class. An item in the game that is not consumed.
- * Instead, it transports Pacman to the location of the paired portal.
- * @see Item
+ * Portal class used to transport LiveActors to the other paired portal
+ * @see InanimateActor
  */
-public class Portal extends Item {
+public class Portal extends InanimateActor {
     private String portalSprite;
     private final Portal portalPair;
     private static final String PORTAL_NAME = "portal";
 
-    // Constructor for portals created without a pair
+    /**
+     * Constructor for the Portal when no pair exists yet
+     * @param sprite    Portal Sprite displayed in the game
+     */
     public Portal(String sprite) {
+        super(sprite);
         this.portalSprite = sprite;
     }
 
-    // Overloaded constructor for portals that will
-    // be constructed with a pair
+    /**
+     * Constructor for the portal when a pair exists already
+     * @param sprite    Portal Sprite displayed in the game
+     * @param pair      Instance to be paired with new portal
+     */
     public Portal(String sprite, Portal pair) {
         this.portalSprite = sprite;
         this.pairPortal = pair;
@@ -22,8 +28,11 @@ public class Portal extends Item {
         pair.setPortalPair(this);
     }
 
-    // Need this to set the
-    public setPortalPair(Portal pair) {
+    /**
+     * Necessary setter function to pair portals together
+     * @param pair  Portal instance paired with another
+     */
+    public void setPortalPair(Portal pair) {
         this.portalPair = pair;
     }
 }
--- pacman/src/PortalFactory.java
+++ pacman/src/PortalFactory.java
@@ -1,4 +1,52 @@
+/**
+ * Factory class for construction of portals; ensures that each portal
+ * is paired with each other.
+ * @see Portal
+ */
+
 public class PortalFactory {
-    private HashMap<String, Integer> portals;
-    private PortalFactory instance;
+    private static String[] portals;
+    public static PortalFactory instance;
+
+    /**
+     * Necessary setter function to put all possible
+     * portals
+     * @param possiblePortals   All possible portals, as String png file names
+     */
+    public void setPortals(String[] possiblePortals) {
+        portals = possiblePortals;
+    }
+
+    /**
+     * Constructor Function for all the portals of the game
+     * @return  List of Portals to put in the board
+     */
+    public Portal[] makePortals() {
+        // First make the portal list, always double the length
+        // of the portal hashmap
+        Portal[] portalList = new Portal[portals.length*2];
+        int i=0;
+
+        // Empty the hashmap since we are only constructiing
+        // portals once
+        for (portal: portalList) {
+            // Now make the portals
+            portalList[i] = new Portal(currPortal);
+            portalList[i+1] = new Portal(currPortal, portalList[i]);
+            i+=2;
+        }
+
+        return portalList;
+    }
+
+    /**
+     * Creates instance of PortalFactory for portal creation
+     * @return  PortalFactory instance
+     */
+    public static synchronized PortalFactory getInstance() {
+        if instance == null {
+            instance = new ServicesFactory();
+        }
+        return instance;
+    }
 }
