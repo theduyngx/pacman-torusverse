022814bf2bd28d6c2249a48d3e30a7c9b718ad5c
theduynguyen <69719457+theduyngx@users.noreply.github.com>
2023-05-24 01:57:00 +1000

    ObjectManager: clean instantiations with XML parser

--- 2D-Map-Editor-master/src/pacman/ObjectManager.java
+++ 2D-Map-Editor-master/src/pacman/ObjectManager.java
@@ -1,9 +1,12 @@
 package pacman;
 import ch.aplu.jgamegrid.Actor;
+import org.xml.sax.SAXException;
 import pacman.utility.GameCallback;
 import pacman.utility.PropertiesLoader;
 import ch.aplu.jgamegrid.Location;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -21,9 +24,6 @@ import java.util.stream.Collectors;
  * @see LiveActor
  */
 public class ObjectManager {
-    // constant initial seed
-    private final static int INIT_SEED = 30006;
-
     // PacMan
     private PacActor pacActor;
     private Actor killedPacActor;
@@ -44,39 +44,24 @@ public class ObjectManager {
     // the constructor for all the portals
     private final PortalFactory portalFactory;
 
-    // the game
-    private final Game game;
     // game callback
     private final GameCallback gameCallback;
-    // random seed
-    private int seed = INIT_SEED;
+
     // current number of pills and gold pieces, which indicate whether player has won or not
     private int numPillsAndGold = 0;
     private boolean isMultiverse = false;
 
     /**
      * Constructor for ObjectManager.
-     * @see Game
      */
-    public ObjectManager(Game game, GameCallback gameCallback) {
-        assert game != null;
-        this.game           = game;
-        this.gameCallback   = gameCallback;
-        this.monsters       = new ArrayList<>();
-        this.items          = new HashMap<>();
-        this.walls          = new HashMap<>();
-        this.pacActorLocations   = new ArrayList<>();
-        this.portalMap      = new HashMap<>();
-        this.portalFactory  = PortalFactory.getInstance();
-    }
-
-    /**
-     * Get the game object; used to retrieve game's grid, which either to update grid's cell or to
-     * get the information about the grid's border to disallow actors moving out of bound.
-     * @return the game
-     */
-    protected Game getGame() {
-        return game;
+    public ObjectManager(GameCallback gameCallback) {
+        this.gameCallback      = gameCallback;
+        this.monsters          = new ArrayList<>();
+        this.items             = new HashMap<>();
+        this.walls             = new HashMap<>();
+        this.pacActorLocations = new ArrayList<>();
+        this.portalMap         = new HashMap<>();
+        this.portalFactory     = PortalFactory.getInstance();
     }
 
     /**
@@ -198,169 +183,48 @@ public class ObjectManager {
 
 
     /**
-     * Parse properties that do not relate to a live actor instantiation. This includes the seed, edible
-     * items, among others available in the properties file.
-     * @param properties the specified properties
-     * @see   Properties
+     * Parse the game's properties file.
+     * @param properties specified properties file
      */
-    public void parseInanimateActor(Properties properties) {
-        seed = Integer.parseInt(properties.getProperty("seed"));
+    public void parseProperties(Properties properties) {
+        // random seed
+        int seed = Integer.parseInt(properties.getProperty("seed"));
         isMultiverse = properties.getProperty("version").contains("multiverse");
-
-        // concern only about locations of edible items
-        ArrayList<InanimateActor.BlockType> blockTypes =
-                new ArrayList<>(Arrays.asList(InanimateActor.BlockType.values()));
-
-        // for each of said item type
-        for (InanimateActor.BlockType blockType : blockTypes) {
-            String name = blockType.toString();
-            String property_name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
-
-            // we check if item is included in properties file
-            if (properties.containsKey(property_name)) {
-                String[] itemLocations = properties.getProperty(property_name).split(";");
-
-                // parse its locations
-                for (String pL : itemLocations) {
-                    String[] pos = pL.split(",");
-                    int posX = Integer.parseInt(pos[0]);
-                    int posY = Integer.parseInt(pos[1]);
-                    Location location = new Location(posX, posY);
-                    Item item = switch(blockType) {
-                        case PILL -> new Pill();
-                        case GOLD -> new Gold();
-                        case ICE  -> new Ice();
-                        default   -> null;
-                    };
-
-                    // if null, then it is not an item
-                    if (item == null) continue;
-
-                    // add to item hashmaps and set game grid's cell
-                    HashLocation.put(items, location, item);
-                    getGame().getGrid().setCell(location, blockType);
-                    if (blockType == InanimateActor.BlockType.PILL || blockType == InanimateActor.BlockType.GOLD)
-                        numPillsAndGold++;
-                }
-            }
+        // parse pacman (comment out to test for torus)
+        pacActor.setAuto(Boolean.parseBoolean(properties.getProperty(
+                pacActor.getName() + PropertiesLoader.AUTO_EXTENSION))
+        );
+        /// SET SEED AND SLOW DOWN TO REDUCE GAME DIFFICULTY
+        pacActor.setSeed(seed);
+        pacActor.setSlowDown(LiveActor.SLOW_DOWN);
+        for (Monster monster : monsters) {
+            monster.setSeed(seed);
+            monster.setSlowDown(LiveActor.SLOW_DOWN);
         }
     }
 
 
     /**
-     * Instantiate the pacman actor. Called in Game constructor.
-     * @param properties properties to parse for pacman
-     * @see   Properties
-     * @see   PacActor
+     * Instantiate all game's objects, given the XML file.
+     * @param xmlFile the specified XML file
      */
-    protected void instantiatePacActor(Properties properties) {
-        // instantiate pacman
-        pacActor = new PacActor(this);
-        pacActor.setSeed(seed);
-        pacActor.setSlowDown(LiveActor.SLOW_DOWN);
-
-        // parse pacman (comment out to test for torus)
-        pacActor.setAuto(Boolean.parseBoolean(properties.getProperty(
-                pacActor.getName() +
-                PropertiesLoader.AUTO_EXTENSION))
-        );
-        String[] pacManLocations = properties.getProperty(
-                pacActor.getName() +
-                PropertiesLoader.LOCATION_EXTENSION
-        ).split(",");
-        int pacManX = Integer.parseInt(pacManLocations[0]);
-        int pacManY = Integer.parseInt(pacManLocations[1]);
-        pacActor.setInitLocation(new Location(pacManX, pacManY));
+    public void instantiateAll(String xmlFile) {
+        XMLParser parser = new XMLParser();
+        try {
+            parser.parseXML(xmlFile, this);
+        } catch (ParserConfigurationException | IOException | SAXException exception) {
+            exception.printStackTrace();
+        }
+        numPillsAndGold = getMandatoryItems().size();
     }
 
     /**
      * Instantiate the pacActor's location based on a given Location instance
-     * @param loc   Location pacActor will spawn ins
+     * @param loc Location pacActor will spawn ins
      */
     protected void instantiatePacActorLoc(Location loc) {
         pacActor = new PacActor(this);
         pacActor.setInitLocation(loc);
-    }
-
-
-    /**
-     * Instantiating monsters. Called in Game constructor.
-     * @param properties properties to parse for monsters
-     * @see   Properties
-     */
-    protected void instantiateMonsters(Properties properties) {
-        // for each monster type
-        ArrayList<Monster.MonsterType> types = new ArrayList<>(Arrays.asList(Monster.MonsterType.values()));
-        for (Monster.MonsterType type : types) {
-            // check if monster type is valid (as in, if type only exists in multiverse but property
-            // states otherwise, then we ignore)
-            if (type.inMultiverse && !isMultiverse) continue;
-            String name = type.toString();
-            String property_name = name + PropertiesLoader.LOCATION_EXTENSION;
-
-            // valid entry
-            if (properties.containsKey(property_name) && !properties.getProperty(property_name).equals("")) {
-                String[] locations = properties.getProperty(property_name).split(";");
-
-                // get all locations of monster
-                for (String loc : locations) {
-                    String[] pos = loc.split(",");
-                    int posX = Integer.parseInt(pos[0]);
-                    int posY = Integer.parseInt(pos[1]);
-                    Location location = new Location(posX, posY);
-                    Monster monster = switch(type) {
-                        case TX5    -> new TX5   (this);
-                        case Troll  -> new Troll (this);
-                        case Orion  -> new Orion (this);
-                        case Alien  -> new Alien (this);
-                        case Wizard -> new Wizard(this);
-                    };
-
-                    // set location and add itself to monster list
-                    monster.setInitLocation(location);
-                    this.monsters.add(monster);
-
-                    /// SET SEED AND SLOW DOWN TO REDUCE GAME DIFFICULTY
-                    monster.setSeed(seed);
-                    monster.setSlowDown(LiveActor.SLOW_DOWN);
-                }
-            }
-        }
-    }
-
-
-    /**
-     * Instantiate the items in the grid and put them in their respective hashmaps. Called in Game constructor.
-     * @param grid the game grid so that the items can be drawn onto
-     * @see   PacManGameGrid
-     */
-    protected void instantiateObjects(PacManGameGrid grid) {
-        for (int col = 0; col < grid.getNumVerticalCells(); col++)
-            for (int row = 0; row < grid.getNumHorizontalCells(); row++) {
-                InanimateActor.BlockType itemType = grid.getMazeArray()[col][row];
-
-                // ignore if location is already occupied
-                Location location = new Location(row, col);
-                if (HashLocation.contain(items, location)) continue;
-
-                // otherwise add
-                switch (itemType) {
-                    case PILL -> {
-                        Pill pill = new Pill();
-                        HashLocation.put(items, location, pill);
-                        numPillsAndGold++;
-                    }
-                    case GOLD -> {
-                        Gold gold = new Gold();
-                        HashLocation.put(items, location, gold);
-                        numPillsAndGold++;
-                    }
-                    case ICE -> {
-                        Ice ice = new Ice();
-                        HashLocation.put(items, location, ice);
-                    }
-                }
-            }
     }
 
 
