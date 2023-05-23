3cbf9a690bdaa64b5dcfd95bed8de95932e28d91
theduynguyen <69719457+theduyngx@users.noreply.github.com>
2023-05-24 02:51:26 +1000

    InanimateObject: BlockType removed, unneeded

--- 2D-Map-Editor-master/src/pacman/InanimateActor.java
+++ 2D-Map-Editor-master/src/pacman/InanimateActor.java
@@ -11,40 +11,6 @@ import ch.aplu.jgamegrid.Location;
  * @see GameActor
  */
 public abstract class InanimateActor extends GameActor {
-    // character representing specific block
-    private static final char WALL_CHAR = 'x';
-    private static final char SPACE_CHAR = ' ';
-    private static final char PILL_CHAR = '.';
-    private static final char GOLD_CHAR = 'g';
-    private static final char ICE_CHAR = 'i';
-    private static final char ERROR_CHAR = '\0';
-
-
-    /**
-     * Enumerated block, or inanimate objects, type. This includes
-     * <ul>
-     *     <li>WALL  - obstructed block which cannot be bypassed
-     *     <li>PILL  - the pill item required to be eaten by pacman to win
-     *     <li>SPACE - the empty space
-     *     <li>GOLD  - the gold piece required to be eaten, but also aggravates monsters
-     *     <li>ICE   - the ice piece not required to be eaten, but freezes monsters
-     *     <li>ERROR - error block (nonexistent)
-     * </ul>
-     */
-    public enum BlockType {
-        WALL(WALL_CHAR),
-        PILL(PILL_CHAR),
-        SPACE(SPACE_CHAR),
-        GOLD(GOLD_CHAR),
-        ICE(ICE_CHAR),
-        ERROR(ERROR_CHAR);
-        public final char BLOCK_CHAR;
-        BlockType(char BLOCK_CHAR) {
-            this.BLOCK_CHAR = BLOCK_CHAR;
-        }
-    }
-
-
     /**
      * Inanimate object constructor. Calls the constructor of GameActor class.
      * @param src the directory for sprite image of the inanimate object
