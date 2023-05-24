d5238e35b4ad10120e219b48d5262a0d38b62bd9
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-05-24 15:08:14 +1000

    XMLParser: Added length and width parser

--- 2D-Map-Editor-master/src/pacman/XMLParser.java
+++ 2D-Map-Editor-master/src/pacman/XMLParser.java
@@ -24,8 +24,14 @@ public class XMLParser {
     public static final int IS_WALL = 1;
 
     // static constants for XML Parsing
+
+    public static final String SIZE = "size";
+    public static final int LENGTH_INDEX = 0;
+    public static final int WIDTH_INDEX = 1;
+    public static final int DIMENSIONS = 2;
     public static final String ROW = "row";
     public static final String CELL = "cell";
+    public static final int INSTANCE_INDEX =0;
     public static final String WALL_TILE = "WallTile";
     public static final String PILL_TILE = "PillTile";
     public static final String GOLD_TILE = "GoldTile";
@@ -57,14 +63,20 @@ public class XMLParser {
         ArrayList<String> colors = new ArrayList<>();
         ArrayList<Location> portalLocations = new ArrayList<>();
 
+        // Create the dimensions, was not used since
+        // dimensions are already instantiated before parsing
+        // int[] dimList = getDimensions(doc);
+        // int width = dimList[WIDTH_INDEX];
+        // int lenght = dimList[LENGTH_INDEX];
+
         // Now loop through every single cell and stores its location
         NodeList rows = doc.getElementsByTagName(ROW);
-        for (int i=0; i < rows.getLength(); i++) {
+        for (int i=0; i< rows.getLength(); i++) {
             Node currRow = rows.item(i);
             Element eElement = (Element) currRow;
             NodeList cellTags = eElement.getElementsByTagName(CELL);
 
-            for (int j=0; j < cellTags.getLength(); j++) {
+            for (int j=0; j<cellTags.getLength(); j++) {
                 Location currLocation = new Location(j, i);
                 String currCell = cellTags.item(j).getTextContent();
 
@@ -110,5 +122,25 @@ public class XMLParser {
         }
         // After this, we want to construct the portals for the object manager
         manager.getPortalFactory().makePortals(manager.getPortals(), colors, portalLocations);
+    }
+
+    /**
+     * Method to extract the length and width from a given Document object
+     * of an XML File; was left unused since the initialization of the Game
+     * object already initializes dimensions.
+     * @param doc   Document object made from XML File
+     * @return      List containing the width and the length of the map
+     */
+    public int[] getDimensions(Document doc) {
+        int[] dimList = new int[DIMENSIONS];
+        // First extract the dimensions
+        Node dimsNode = doc.getElementsByTagName(SIZE).item(INSTANCE_INDEX);
+        String[] dims = dimsNode.getTextContent().strip().split("\n\s+");
+        int length = Integer.parseInt(dims[LENGTH_INDEX]);
+        int width = Integer.parseInt(dims[WIDTH_INDEX]);
+        // Then put dimensions into the list
+        dimList[LENGTH_INDEX] = length;
+        dimList[WIDTH_INDEX] = width;
+        return dimList;
     }
 }
